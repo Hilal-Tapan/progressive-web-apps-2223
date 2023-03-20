1bbea5ae417296bc4ae602d98877560cbf24ea1f
“Hilal-Tapan“ <hilalt98@hotmail.com>
2023-03-20 14:29:48 +0100

    params

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ const port = 3000;
 // routes
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
 
+app.get('/quote/:quotes', (req, res) => {
+    res.send(`${req.params.quotes}`)
 })
 
 // start webserver
 app.listen(port, () => {
-    console.log('localhost running')
+    console.log(`localhost running on https://localhost:${port}`)
 });
