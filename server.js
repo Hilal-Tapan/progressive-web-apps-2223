843a7aae768aef07afea98a907f903b900b5b54c
“Hilal-Tapan“ <hilalt98@hotmail.com>
2023-03-24 14:32:12 +0100

    alles

--- server.js
+++ server.js
@@ -1,18 +1,71 @@
 const express = require('express')
+const axios = require('axios');
 
 const app = express();
 const port = 3000;
 
-// routes
+app.use(express.static('public'))
+
+
+//setting view engine to ejs
+app.set("view engine", "ejs");
+
+// Home page
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    let data;
+    axios.get('https://opensheet.elk.sh/12nr4W-RHpvhnw76MCZZtujYHqP1qIU28ExM4oXQfzys/blad1')
+        .then((response) => {
+            console.log('result', response.data)
+            data = response.data
+            res.render('index', {
+                title: 'Home',
+                data: data
+            });
+
+        }).catch((error) => {
+            console.log('error', error)
+        })
 })
 
-app.get('/quote/:quotes', (req, res) => {
-    res.send(`${req.params.quotes}`)
+// Quotes page
+app.get('/quotes/:id', (req, res) => {
+    let data;
+    axios.get('https://opensheet.elk.sh/12nr4W-RHpvhnw76MCZZtujYHqP1qIU28ExM4oXQfzys/blad1')
+      .then((response) => {
+        console.log('result', response.data);
+        data = response.data;
+        const filteredQuotes = data.filter(quote => quote.id === req.params.id); // filter the quotes based on the id parameter
+        res.render('quotes', {
+          title: 'Random Quote',
+          data: filteredQuotes // render only the filtered quotes
+        });
+      })
+      .catch((error) => {
+        console.log('error', error);
+      });
+  });
+  
+
+// About pagina
+app.get('/about', (req, res) => {
+
+    res.render('about', {
+        title: 'About',
+        pageTitle: 'About Design Quotes'
+    });
 })
 
 // start webserver
 app.listen(port, () => {
     console.log(`localhost running on https://localhost:${port}`)
 });
+
+// Make sure to export the router so it becomes available on imports
+module.exports = app;
+
+
+
+// AANTEKENINGEN
+// app.get('/quote/:quotes', (req, res) => {
+//     res.send(`${req.params.quotes}`)
+// })
